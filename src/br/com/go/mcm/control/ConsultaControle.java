fbaec33454e8c0631e170ed95fc675efcbacc035
Gil Carelli <gmcarelli@gmail.com>
2016-05-25 17:13:29 -0300

    atualizando

--- src/br/com/go/mcm/control/ConsultaControle.java
+++ src/br/com/go/mcm/control/ConsultaControle.java
@@ -7,6 +7,8 @@ package br.com.go.mcm.control;
 
 import br.com.go.mcm.model.Consulta;
 import br.com.go.mcm.model.Medico;
+import br.com.go.mcm.model.Paciente;
+import br.com.go.mcm.model.Pessoa;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,11 +21,12 @@ public class ConsultaControle extends QueryHelper {
 
     public String gerarQueryAgendarConsulta(Consulta consulta) {
         this.query = "INSERT INTO consulta (prontuarioPaciente, crmMedico, dataConsulta, "
-                + "diagnosticoConsulta, exameSolicitado) VALUES ("
+                + "diagnosticoConsulta, exameSolicitado, isRetorno) VALUES ("
                 + consulta.getPaciente().getProntuarioPaciente() + ", '"
                 + consulta.getMedico().getCrmMedico() + "', '"
                 + consulta.getDataConsulta() + "', '" + consulta.getDiagnostico() + "', '"
-                + consulta.getExamesSolicitado() + "');";
+                + consulta.getExamesSolicitados() + "', '"
+                +consulta.getIsRetorno() + "');";
 
         return this.query;
     }
@@ -32,8 +35,9 @@ public class ConsultaControle extends QueryHelper {
         this.query = "UPDATE consulta SET prontuarioPaciente = " + consulta.getPaciente().getProntuarioPaciente() + ", "
                 + "crmMedico = '" + consulta.getMedico() + "', dataConsulta = '" + consulta.getDataConsulta() + "', "
                 + "diagnosticoConsulta = '" + consulta.getDiagnostico() + "', "
-                + "exameSolicitado = '" + consulta.getExamesSolicitado() + "', "
-                + "statusConsulta = '" + consulta.getStatusConsulta() + "';";
+                + "exameSolicitado = '" + consulta.getExamesSolicitados() + "', "
+                + "statusConsulta = '" + consulta.getStatusConsulta() + "', "
+                + "isRetorno = '" + consulta.getIsRetorno() + "', ";
 
         return this.query;
     }
@@ -44,13 +48,33 @@ public class ConsultaControle extends QueryHelper {
     }
 
     public List<Consulta> listarConsulta() throws SQLException {
-        List<Consulta> listaConsulta = null;
-       
         
-        this.query = "SELECT * FROM consulta "
-                + "NATURAL JOIN medico m"
-                + "NATURAL JOIN paciente paci "
-                ;
+        List<Consulta> listaConsulta = new ArrayList<>();       
+        
+        this.query = "SELECT idConsulta, statusConsulta, dataConsulta, diagnostico, examesSolicitados, isRetorno"
+                + " FROM consulta NATURAL JOIN"
+                + "("
+                + "SELECT idPessoa, nomePessoa "
+                + "FROM Pessoa pes NATURAL JOIN  paciente paci"
+                + ")"
+                + "("
+                + "SELECT nomePessoa AS nomeMedico "
+                + "FROM Pessoa NATURAL JOIN Medico "
+                + ")"
+                + ")"
+                + "SELECT logradouroEndereco, numeroEndereco, complementoEndereco, bairroEndereco, cidadeEndereco, estadoEndereco, CEPEndereco"
+                + "FROM Endereco NATURAL JOIN Paciente"
+                + ")"
+                + "("
+                + "SELECT telefoneResidencial, telefoneComercial, telefoneCelular"
+                + "FROM Telefone NATRURAL JOIN Paciente"
+                + ")"
+                + "("
+                + "SELECT  telefoneResidencial AS telefoneResidencialMedico, telefoneComercial AS telefoneComercialMedico, telefoneCelular AS telefoneCelularMedico"
+                + "FROM Telefone NATURAL JOIN Medico "
+                + ")"
+              
+                 ;
         
         
         this.prepStatement = this.mySqlControle.getConnection().prepareStatement(query);
@@ -58,11 +82,22 @@ public class ConsultaControle extends QueryHelper {
         this.resultSet = this.prepStatement.executeQuery();
         
         while (this.resultSet.next()) {
-             //Medico medico = new Medico()
+            Pessoa pessoa = new Pessoa.Builder().contruir();          
+            Medico medico = new Medico();
+            Paciente paciente = new Paciente.Builder().contruir();
             Consulta consulta = new Consulta.Builder()
                     .idConsulta(this.resultSet.getInt("idConsulta"))
-                    .statusConsulta("")
+                    .statusConsulta(this.resultSet.getString("statusConsulta"))
+                    .dataConsulta(this.resultSet.getDate("dataConsulta"))
+                    .examesSolicitados(this.resultSet.getString("examesSolicitados"))
+                    .diagnostico(this.resultSet.getString("diagnostico"))
+                    .isRetorno(this.resultSet.getString("isRetorno").charAt(0))
+                    .paciente(paciente)
+                    .medico(medico)
                     .construir();
+                    
+           listaConsulta.add(consulta);
+                    
         }
 
         return listaConsulta;
